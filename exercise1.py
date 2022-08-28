1b10a8b6e6832a8da08e13c86ebdbefbd73aff54
viet <nguyentaiviet9@gmai.com>
2022-08-28 11:19:33 +0700

    viet submission

---
+++ exercise1.py
@@ -0,0 +1,7 @@
+#Insert the missing part of the code below to output "Hello World"
+print("hello wrold")
+"""This example misses indentations to be correct.
+
+Insert the missing indentation to make the code correct:"""
+if 5 > 2:
+    print("Five is greater than two!")

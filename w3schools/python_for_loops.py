494ab8ab694be17400980961d37e577522f36e1d
viet <nguyentaiviet9@gmai.com>
2022-08-30 08:54:56 +0700

    viet fix

---
+++ w3schools/python_for_loops.py
@@ -0,0 +1,19 @@
+# exercise1
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+# exercise2
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        continue
+    print(x)
+# exercise3
+for x in range(6):
+    print(x)
+# exercise4
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        break
+    print(x)
---
+++ w3schools/python_while_loops.py
@@ -0,0 +1,25 @@
+# exercise1
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+# exercise2
+i = 1
+while i < 6:
+    if i == 3:
+        break
+    i += 1
+# exercise3
+i = 0
+while i < 6:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
+# exercise4
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+else:
+    print("i is no longer less than 6")

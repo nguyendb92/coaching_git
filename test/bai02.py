808fc52f02a486253fd2bfd22b3c29638a5777d3
viet <nguyentaiviet9@gmai.com>
2022-09-11 10:42:35 +0700

    chau viet nop lai

---
+++ test/bai02.py
@@ -0,0 +1,5 @@
+a = input()
+list = a.split(",")
+tuple = tuple(list)
+print('List : ',list)
+print('Tuple : ',tuple)
---
+++ test/bai05.py
@@ -0,0 +1,9 @@
+for i in range(1, 51):
+    if i % 15 is 0:
+        print(i,"FizzBuzz")
+    elif i % 3 is 0:
+        print(i,"Fizz")
+    elif i % 5 is 0:
+        print(i,"Buzz")
+    else:
+        print(i)

577704ef981d92d374d2878155ed481b622a1ff0
viet <nguyentaiviet9@gmai.com>
2022-09-21 18:28:05 +0700

    chau nop lai bai 7

--- 10bt/bai07.py
+++ 10bt/bai07.py
@@ -1,6 +1,7 @@
-def find():
-    a = [j for j in range(0, 1000) if '3' in str(j)]
+def find(x):
+    a = [j for j in range(x[0], x[-1] + 1) if '3' in str(j)]
     return a
 
 
+m = [int(n) for n in input().split()]
 print(find())
